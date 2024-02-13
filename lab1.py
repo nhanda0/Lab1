79fc7a77f1561b6585faf84c0cca9c34c2dcc6b4
nhanda0 <159860532+nhanda0@users.noreply.github.com>
2024-02-12 22:42:51 -0500

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -2,5 +2,5 @@ def calculate_age():
     birth_year = int(input("Enter your birth year: "))
     current_year = 2024  # Assuming the current year is 2024
     age = current_year - birth_year 
-    print("your age is:",age)
+    print("your age is:",age) 
     
